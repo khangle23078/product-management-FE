825489ca4900ccb9116918786912be1be902075c
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2023-12-09 01:12:17 +0700

    style: create homepage

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Product } from '../interfaces/product'
 import { Card } from 'antd'
+import { useAppSelector } from '../hooks'
 
 type ProductCardProps = {
   product: Product
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  const { isAuthentication } = useAppSelector((state) => state.auth)
   return (
-    <Card>
-      <img src={product.image.url} />
+    <Card className='w-[200px]'>
+      <img src={product.image.url} width={150} />
       <p>{product.name}</p>
-      <p>{product.price}</p>
+      <p>{isAuthentication ? product.price : "Liên hệ"}</p>
     </Card>
   )
 }
--- src/pages/client/HomePage.tsx
+++ src/pages/client/HomePage.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Header from '../../components/Header'
 import { Card, Typography } from 'antd'
+import { useGetProductsQuery } from '../../services/product'
+import { Product } from '../../interfaces/product'
+import ProductCard from '../../components/ProductCard'
 
 const { Title } = Typography
 const HomePage: React.FC = () => {
+  const { data: response } = useGetProductsQuery()
+  const products = response?.data
+
   return (
     <>
       <Header />
@@ -11,6 +17,11 @@ const HomePage: React.FC = () => {
         <div className='h-screen max-w-6xl py-2 mx-auto'>
           <Card>
             <Title level={4}>Danh sách sản phẩm</Title>
+            <div className='grid grid-cols-4 gap-4'>
+              {products?.map((product: Product) => {
+                return <ProductCard product={product} />
+              })}
+            </div>
           </Card>
         </div>
       </div>
